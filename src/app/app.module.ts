bc9a6bf5b580108efba0d07ce4a294c3dc813d75
pgsilva <31107814+pgsilva@users.noreply.github.com>
2018-05-25 21:42:54 -0300

    Entrega 2 package model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,111 +6,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { AfricaPage } from '../pages/africa/africa';
-import { AmericaPage } from '../pages/america/america';
-import { AsiaPage } from '../pages/asia/asia';
-import { EuropaPage } from '../pages/europa/europa';
-import { OceaniaPage } from '../pages/oceania/oceania';
-import { AfricaDoSulPage } from '../pages/africa-do-sul/africa-do-sul';
-import { EgitoPage } from '../pages/egito/egito';
-import { CongoPage } from '../pages/congo/congo';
-import { MadagascarPage } from '../pages/madagascar/madagascar';
-import { NigeriaPage } from '../pages/nigeria/nigeria';
-import { BrasilPage } from '../pages/brasil/brasil';
-import { ArgentinaPage } from '../pages/argentina/argentina';
-import { ChilePage } from '../pages/chile/chile';
-import { UsaPage } from '../pages/usa/usa';
-import { UruguaiPage } from '../pages/uruguai/uruguai';
-import { JapaoPage } from '../pages/japao/japao';
-import { CoreiaPage } from '../pages/coreia/coreia';
-import { TailandiaPage } from '../pages/tailandia/tailandia';
-import { IndiaPage } from '../pages/india/india';
-import { ChinaPage } from '../pages/china/china';
-import { FrancaPage } from '../pages/franca/franca';
-import { AlemanhaPage } from '../pages/alemanha/alemanha';
-import { FinlandiaPage } from '../pages/finlandia/finlandia';
-import { DinamarcaPage } from '../pages/dinamarca/dinamarca';
-import { InglaterraPage } from '../pages/inglaterra/inglaterra';
-import { AustraliaPage } from '../pages/australia/australia';
-import { NovaZelandiaPage } from '../pages/nova-zelandia/nova-zelandia';
-import { FujiPage } from '../pages/fuji/fuji';
-import { SamoaPage } from '../pages/samoa/samoa';
-import { IndonesiaPage } from '../pages/indonesia/indonesia';
+import { PaisesPageModule } from '../pages/paises/paises.module';
+import { PaisesPage } from '../pages/paises/paises';
+import { PaisPage } from '../pages/pais/pais';
+import { PaisPageModule } from '../pages/pais/pais.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    HomePage,
-    AfricaPage,
-    AmericaPage,
-    AsiaPage,
-    EuropaPage,
-    OceaniaPage,
-    AfricaDoSulPage,
-    EgitoPage,
-    CongoPage,
-    MadagascarPage,
-    NigeriaPage,
-    BrasilPage,
-    ArgentinaPage,
-    ChilePage,
-    UsaPage,
-    UruguaiPage,
-    CoreiaPage,
-    TailandiaPage,
-    IndiaPage,
-    ChinaPage,
-    JapaoPage,
-    FrancaPage,
-    AlemanhaPage,
-    FinlandiaPage,
-    DinamarcaPage,
-    InglaterraPage,
-    NovaZelandiaPage,
-    FujiPage,
-    SamoaPage,
-    IndonesiaPage,
-    AustraliaPage
+    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    PaisesPageModule,
+    PaisPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    AfricaPage,
-    AmericaPage,
-    AsiaPage,
-    EuropaPage,
-    OceaniaPage,
-    AfricaDoSulPage,
-    EgitoPage,
-    CongoPage,
-    MadagascarPage,
-    NigeriaPage,
-    BrasilPage,
-    ArgentinaPage,
-    ChilePage,
-    UsaPage,
-    UruguaiPage,
-    JapaoPage,
-    CoreiaPage,
-    TailandiaPage,
-    IndiaPage,
-    ChinaPage,
-    FrancaPage,
-    AlemanhaPage,
-    FinlandiaPage,
-    DinamarcaPage,
-    InglaterraPage,
-    AustraliaPage,
-    NovaZelandiaPage,
-    FujiPage,
-    SamoaPage,
-    IndonesiaPage
+    PaisesPage,
+    PaisPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AfricaPage } from '../africa/africa';
-import { AmericaPage } from '../america/america';
-import { AsiaPage } from '../asia/asia';
-import { EuropaPage } from '../europa/europa';
-import { OceaniaPage } from '../oceania/oceania';
+import { PaisesPage } from '../paises/paises';
 
 @Component({
   selector: 'page-home',
@@ -17,17 +13,7 @@ export class HomePage {
 
   }
 
-  listarPaises(){
-    if(this.continente == 'Africa'){
-      this.navCtrl.push(AfricaPage)
-    } else if(this.continente == 'Americas'){
-      this.navCtrl.push(AmericaPage)
-    } else if(this.continente == 'Asia'){
-      this.navCtrl.push(AsiaPage)
-    } else if(this.continente == 'Europa'){
-      this.navCtrl.push(EuropaPage)
-    } else if(this.continente == 'Oceania'){
-      this.navCtrl.push(OceaniaPage)
-    } 
+  listarPaises() {
+    this.navCtrl.push(PaisesPage, {"continente": this.continente})
   }
 }
